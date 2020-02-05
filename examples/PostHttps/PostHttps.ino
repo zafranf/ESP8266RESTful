94cdf00f9f999282195be0ab2bc36ce2f5261914
zafranf <zafranfadilah@gmail.com>
2020-02-05 12:45:26 +0700

    add post example

--- examples/PostHttps/PostHttps.ino
+++ examples/PostHttps/PostHttps.ino
@@ -5,18 +5,20 @@
 
 #include "ESP8266RESTful.h"
 
-ESP8266RESTful REST("http://ngetes.com");
+ESP8266RESTful REST("https://ngetes.com");
 
 void setup()
 {
   Serial.println();
   Serial.begin(9600);
   REST.begin("ssid", "password");
+  REST.setSecureConnection(true);
+  REST.setFingerprint("C7 D1 21 64 EB 06 84 56 B9 BA 19 EB 69 74 53 76 1A AE BD 99");
 }
 
 void loop()
 {
-  char *data = "{\"method\":\"post\",\"scheme\":\"http\"}";
+  char *data = "{\"method\":\"post\",\"scheme\":\"https\"}";
   int statusCode = REST.post("/post", data);
   Serial.print("Status code from server: ");
   Serial.println(REST.getStatusCode());
